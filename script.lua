465d696eab23b5dd6e7abab25617e7ee22418e17
TomVerez531000 <86794107+TomVerez531000@users.noreply.github.com>
2022-08-12 11:15:05 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,11 +1,14 @@
-game.Workspace.Val.Value = false
-game.Workspace.Val:Destroy()
+if game.Workspace.Val then
+  game.Workspace.Val.Value = false
+  game.Workspace.Val:Destroy()
+end
+
 local Val = Instance.new("BoolValue", game.Workspace)
 Val.Name = "Val"
 Val.Value = true
 
 while true do
   if Val.Value == true then
-    
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
   end
 end
